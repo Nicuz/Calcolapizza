7b0e5fc026448696fd21825ba0fc11a13231b4a9
Nicuz <nico.majorana@gmail.com>
2019-06-26 13:58:49 +0200

    Add padding property

--- lib/ui/widgets/rounded_button.dart
+++ lib/ui/widgets/rounded_button.dart
@@ -6,22 +6,27 @@ class RoundedButton extends StatelessWidget {
   final Color textColor;
   final double borderRadius;
   final Function onPressed;
+  final EdgeInsets padding;
 
   RoundedButton(
       {this.text,
       this.color,
       this.textColor = Colors.white,
       this.borderRadius = 100,
-      this.onPressed});
+      this.onPressed,
+      this.padding = const EdgeInsets.all(15.0)});
 
   @override
   Widget build(BuildContext context) {
     return RaisedButton(
       color: this.color,
-      child: Text(
-        this.text,
-        style: TextStyle(
-          color: this.textColor,
+      child: Padding(
+        padding: this.padding,
+        child: Text(
+          this.text,
+          style: TextStyle(
+            color: this.textColor,
+          ),
         ),
       ),
       shape: RoundedRectangleBorder(
