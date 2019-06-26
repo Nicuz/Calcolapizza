d2e7ead27b966a7cc1f95d819c08234dd8468aee
Nicuz <nico.majorana@gmail.com>
2019-06-26 15:46:45 +0200

    Changed bottom bar package

--- lib/providers/navigation_provider.dart
+++ lib/providers/navigation_provider.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class NavigationProvider with ChangeNotifier {
+  int bottomBarIndex = 0;
   final PageController _pageController = PageController(initialPage: 0);
 
   PageController get pageController => _pageController;
 
   set setPage(int index) {
+    bottomBarIndex = index;
     _pageController.animateToPage(index,
         duration: Duration(milliseconds: 300), curve: Curves.ease);
+    notifyListeners();
   }
 }
--- lib/ui/widgets/bottombar.dart
+++ lib/ui/widgets/bottombar.dart
@@ -1,8 +1,8 @@
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
 import 'package:calcolapizza/app_localizations.dart';
-import 'package:cuberto_bottom_bar/cuberto_bottom_bar.dart';
+import 'package:calcolapizza/providers/navigation_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:calcolapizza/providers/navigation_provider.dart';
 import 'package:provider/provider.dart';
 
 class BottomBar extends StatelessWidget {
@@ -13,30 +13,51 @@ class BottomBar extends StatelessWidget {
 
     return Container(
       margin: EdgeInsets.fromLTRB(8, 0, 8, 8),
-      child: CubertoBottomBar(
-        barBorderRadius: BorderRadius.circular(10),
-        inactiveIconColor: Colors.grey,
-        tabStyle: CubertoTabStyle.STYLE_FADED_BACKGROUND,
-        initialSelection: 0,
-        tabs: <TabData>[
-          TabData(
-            iconData: FontAwesomeIcons.calculator,
-            title: "Calcolapizza",
-            tabColor: Colors.deepOrange,
-          ),
-          TabData(
-            iconData: FontAwesomeIcons.pizzaSlice,
-            title: AppLocalizations.of(context).translate("savedDoughsTab"),
-            tabColor: Colors.teal,
-          ),
-          TabData(
-              iconData: FontAwesomeIcons.infoCircle,
-              title: AppLocalizations.of(context).translate("aboutTab"),
-              tabColor: Colors.amber),
-        ],
-        onTabChangedListener: (int position, String title, Color color) {
-          navigationProvider.setPage = position;
+      child: BubbleBottomBar(
+        iconSize: 20,
+        opacity: .2,
+        currentIndex: navigationProvider.bottomBarIndex,
+        onTap: (int index) {
+          navigationProvider.setPage = index;
         },
+        borderRadius: BorderRadius.circular(10),
+        elevation: 8,
+        items: <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+              backgroundColor: Colors.deepOrange,
+              icon: Icon(
+                FontAwesomeIcons.calculator,
+                color: Colors.grey,
+              ),
+              activeIcon: Icon(
+                FontAwesomeIcons.calculator,
+                color: Colors.deepOrange,
+              ),
+              title: Text("Calcolapizza")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.teal,
+              icon: Icon(
+                FontAwesomeIcons.pizzaSlice,
+                color: Colors.grey,
+              ),
+              activeIcon: Icon(
+                FontAwesomeIcons.pizzaSlice,
+                color: Colors.teal,
+              ),
+              title: Text(
+                  AppLocalizations.of(context).translate("savedDoughsTab"))),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.amber,
+              icon: Icon(
+                FontAwesomeIcons.infoCircle,
+                color: Colors.grey,
+              ),
+              activeIcon: Icon(
+                FontAwesomeIcons.infoCircle,
+                color: Colors.amber,
+              ),
+              title: Text(AppLocalizations.of(context).translate("aboutTab"))),
+        ],
       ),
     );
   }
